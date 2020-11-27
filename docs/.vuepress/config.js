ff2a3823d8d333b68f35dd598dbdbcd19a997c58
e <18790336164@163.com>
2020-11-27 13:27:50 +0800

    修改中: 修改评论插件以及个人中心

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -172,11 +172,11 @@ module.exports = {
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
+          clientID: 'e65010a19c0c39a5e0e2',
+          clientSecret: '2af55368820518df7018d28dded1bf4e6f73b0c9',
           repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          owner: 'dakang824', // GitHub仓库所有者
+          admin: ['dakang824'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
