a224cf61a72d50da401246fca57173d78b7b8af1
e <18790336164@163.com>
2020-12-01 17:01:41 +0800

    添加收藏vue链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/EB-logo.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: 'dakang824', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
@@ -64,7 +64,7 @@ module.exports = {
       link: 'https://github.com/dakang824' // 可选的
     },
     blogger: { // 博主信息，显示在首页侧边栏
-      avatar: 'https://avatars0.githubusercontent.com/u/24062723?s=460&u=35e97fb05cc0516459684f49f5ee21ae1ba8f7c2&v=4',
+      avatar: 'https://thirdwx.qlogo.cn/mmopen/vi_32/5HjjnLYsOvWaQiajNs4Fia4KUE1MF2jmux2U6oDwJicU6TwfTLOqBeUmFmzu8aRfAR8j8y146OvdgGKSnwPodlsxQ/132',
       name: 'YU Kang',
       slogan: '前端界的小学生'
     },
--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,3 +1,4 @@
+
 module.exports = [
   {text: '首页', link: '/'},
   {
@@ -54,7 +55,7 @@ module.exports = [
     items: [
       {text: '网站', link: '/pages/beb6c0bd8a66cea6/'},
       {text: '资源', link: '/pages/eee83a9211a70f9d/'},
-      {text: 'Vue资源', link: '/pages/12df8ace52d493f6/'},
+      {text: 'Vue资源', link: '/pages/12df8ace52d493f6'},
     ]
   },
   {
